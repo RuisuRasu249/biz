ce39597cc39bbb31c4908f3f8460833dce96ae60
Luis Ras <Ras-L@ulster.ac.uk>
2024-11-23 00:01:41 +0000

    Front end Pagination

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,8 +2,18 @@ import jsonData from '../assets/data.json';
 
 export class DataService{
     
-    getBusiness(){
-        return jsonData;
+    //Implement PAGINATION
+    pageSize: number = 3;
+
+    getBusinesses(page: number){
+        let pageStart = (page - 1) * this.pageSize;
+        let pageEnd = pageStart + this.pageSize;
+        
+        return jsonData.slice(pageStart, pageEnd);
+    }
+
+    getLastPageNumber(){
+        return Math.ceil(jsonData.length / this.pageSize);
     }
 
 }
