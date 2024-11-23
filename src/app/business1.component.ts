da43e10738c9486f9912b1e1de64aecb23c3b6cf
Luis Ras <Ras-L@ulster.ac.uk>
2024-11-23 12:32:29 +0000

    Using Coordinate Data with Google Maps

--- src/app/business1.component.ts
+++ src/app/business1.component.ts
@@ -2,22 +2,46 @@ import { Component } from '@angular/core';
 import { RouterOutlet, ActivatedRoute } from '@angular/router';
 import { DataService } from './data.service';
 import { CommonModule } from '@angular/common';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'business',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, GoogleMapsModule],
   providers: [DataService],
   templateUrl: './business1.component.html',
-  styleUrl: './business1.component.css'
+  styleUrls: ['./business1.component.css'] // Note: styleUrls instead of styleUrl
 })
 export class Business1Component {
   business_list: any;
 
+  business_lat: any;
+  business_lng: any;
+  map_options: google.maps.MapOptions = {};
+  map_locations: any[] = [];
+
   constructor(public dataService: DataService, private route: ActivatedRoute){}
 
   ngOnInit(){
-    this.business_list = this.dataService.getBusiness(
-        this.route.snapshot.paramMap.get('name')
-    );
-}
+    this.business_list = this.dataService.getBusiness(this.route.snapshot.paramMap.get('name'));
+    
+    if (this.business_list && this.business_list.length > 0 && this.business_list[0].location && this.business_list[0].location.coordinates) {
+      this.business_lat = this.business_list[0].location.coordinates[0];
+      this.business_lng = this.business_list[0].location.coordinates[1];
+
+      this.map_locations.push({
+        lat: this.business_lat,
+        lng: this.business_lng
+      })
+
+      this.map_options = {
+        mapId: "DEMO_MAP_ID",
+        center: {
+          lat: this.business_lat,
+          lng: this.business_lng
+        },
+        zoom: 13
+      };
+    }
+  }
 }
